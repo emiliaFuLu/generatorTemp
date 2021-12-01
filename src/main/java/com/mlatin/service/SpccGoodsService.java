1968510e207a8e4894e8d427aec8a28551c3b12c
fuluZzz <1039897786@qq.com>
2021-12-01 19:37:11 +0800

    feat:添加根据年份查询接口

--- src/main/java/com/mlatin/service/SpccGoodsService.java
+++ src/main/java/com/mlatin/service/SpccGoodsService.java
@@ -3,11 +3,15 @@ package com.mlatin.service;
 import com.mlatin.codegen.mapper.SpccGoods;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  *  服务类
  * @author Lu Fu
  * @since 2020-11-19
  */
 public interface SpccGoodsService extends IService<SpccGoods> {
+
+    List<SpccGoods> getByYear(String year);
 
 }
--- src/main/java/com/mlatin/service/impl/SpccGoodsServiceImpl.java
+++ src/main/java/com/mlatin/service/impl/SpccGoodsServiceImpl.java
@@ -1,13 +1,19 @@
 package com.mlatin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.mlatin.codegen.mapper.BaseModel;
 import com.mlatin.codegen.mapper.SpccGoods;
 import com.mlatin.codegen.dao.SpccGoodsDao;
 import com.mlatin.service.SpccGoodsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
- *  服务实现类
+ * 服务实现类
+ *
  * @author Lu Fu
  * @since 2020-11-19
  */
@@ -15,4 +21,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SpccGoodsServiceImpl extends ServiceImpl<SpccGoodsDao, SpccGoods> implements SpccGoodsService {
 
+    @Override
+    public List<SpccGoods> getByYear(String year) {
+        LambdaQueryWrapper<SpccGoods> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SpccGoods::getYear, year)
+                .eq(BaseModel::getArchive, false);
+        
+        return baseMapper.selectList(wrapper);
+    }
 }
